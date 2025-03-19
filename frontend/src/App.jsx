96985a46493b49c8baad43d69f60cca3b8feb54f
Anil Verma <rajat.asoftech@gmail.com>
2025-03-19 18:48:07 +0530

    add contact us route only
    add contact us route only
    and create contact page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,45 +10,36 @@ import Technology from "./Dashboard/pages/TechnologyPage/Technology"
 import NewAddTechnology from "./Dashboard/pages/TechnologyPage/NewAddTechnology";
 import CategoryTechnology from "./Dashboard/pages/TechnologyPage/categoryTechnology";
 import SignInForm from "./Dashboard/components/auth/SignInForm"
+import ContactUs from './Pages/ContactUs';
 
 
 
 export default function App() {
-    return (
+  return (
 
-        <BrowserRouter>
+    <BrowserRouter>
 
-            <Routes>
-                  <Route path="/" element={<OutLet />}>
-                    <Route index element={<Home />} />
-                    <Route path='/about' element={<About />} />
+      <Routes>
+        <Route path="/" element={<OutLet />}>
+          <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact-us' element={<ContactUs />} />
 
-                  </Route>
-
-
-             
-
-
-            </Routes>
+        </Route>
+      </Routes>
 
 
-            <Routes>
-                      {/* Dashboard Layout */}
-                      
-                    
-                      <Route element={<AppLayout />}>
-                        <Route index path="/Dashboard" element={<DashboardHome />} />
-                        <Route  path="/Technology" element={<Technology />} />
-                        <Route  path="/AddNewTechnology" element={<NewAddTechnology />} />
-                        <Route  path="/CategoryTechnology" element={<CategoryTechnology />} />
-                        </Route>
-                        
-                        <Route  path="/AISLogin" element={<SignInForm />} />
+      <Routes>
+        {/* Dashboard Layout */}
+        <Route element={<AppLayout />}>
+          <Route index path="/Dashboard" element={<DashboardHome />} />
+          <Route path="/Technology" element={<Technology />} />
+          <Route path="/AddNewTechnology" element={<NewAddTechnology />} />
+          <Route path="/CategoryTechnology" element={<CategoryTechnology />} />
+        </Route>
+        <Route path="/AISLogin" element={<SignInForm />} />
+      </Routes>
+    </BrowserRouter>
 
-            </Routes>
-
-
-        </BrowserRouter>
-
-    )
+  )
 }
