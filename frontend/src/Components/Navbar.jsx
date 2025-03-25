e4a78a14dae2c34a08cb1b4c4c03f8dabea24e46
atrixit2025 <rajat.asoftech@gmail.com>
2025-03-26 01:32:01 +0530

    Update Navbar.jsx

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -52,7 +52,7 @@ export default function Navbar() {
           </div>
 
           <div className="absolute inset-y-0 right-0 xl:flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 hidden">
-            <Button className="relative" mybtn={"Let's Talk"} btnLink={"/contact-us"} />Stashed changes
+            <Button className="relative" mybtn={"Let's Talk"} btnLink={"/contact-us"} />
           </div>
 
           <div className="left-0 flex items-center justify-end xl:hidden">
