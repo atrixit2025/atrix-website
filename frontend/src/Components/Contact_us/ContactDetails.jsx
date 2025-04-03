78a3cbb7a97a9191763979d77b5caf56cfab3470
Anil Verma <rajat.asoftech@gmail.com>
2025-04-04 00:50:36 +0530

    changes

--- frontend/src/Components/Contact_us/ContactDetails.jsx
+++ frontend/src/Components/Contact_us/ContactDetails.jsx
@@ -36,7 +36,6 @@ const ContactDetails = () => {
           <div className="contact-card flex flex-col bg-[#444343] py-10 px-8 rounded-3xl shadow-2xl shadow-white/5 ">
             <h3 className="text-2xl font-bold ">{item.title}</h3>
             <p className=" flex-1 mt-4 mb-10">{item.desc}</p>
-
             <Link target={item.btn_name === "Google Map" ? "_blank" : '_self'} to={item.link} className="font-bold pt-4 flex items-center  cursor-pointer hover:text-(--blue) self-start  group ">
             {item.btn_name} <span className='border border-white/45 ml-2 flex justify-center items-center h-6 w-6 rounded-full  -rotate-45 text-[var(--blue)] group-hover:rotate-1 group-hover:bg-(--blue) group-hover:text-(--white) group-hover:border-(--blue)   duration-300' ><FaArrowRight /></span></Link>
           </div>
