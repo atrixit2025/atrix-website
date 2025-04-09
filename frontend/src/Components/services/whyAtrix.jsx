0905d6bb32b8abcb51fef8bb2ba07eb1164f4a91
Anil Verma <rajat.asoftech@gmail.com>
2025-04-10 01:35:24 +0530

    Anil-as

---
+++ frontend/src/Components/services/whyAtrix.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const whyAtrix = () => {
+  return (
+    <div className='why-atrix'>
+        <div className='container ' ></div>
+
+    </div>
+  )
+}
+
+export default whyAtrix
--- frontend/src/Pages/Service.jsx
+++ frontend/src/Pages/Service.jsx
@@ -3,13 +3,16 @@ import ServiceHeroSlider from '../Components/services/ServiceHeroSlider'
 import ProcessSteps from '../Components/services/ProcessSteps'
 import ServicesCards from '../Components/services/ServicesCards'
 import OurPortfolio from '../Components/OurPortfolio'
+import WhyNeedBranding from '../Components/services/WhyNeedBranding'
 
 const Service = () => {
     return (
         <div className="pt-[150px] ">
             <ServiceHeroSlider></ServiceHeroSlider>
-            <ProcessSteps></ProcessSteps>
+            {/* <ProcessSteps></ProcessSteps> */}
+
             <ServicesCards></ServicesCards>
+            <WhyNeedBranding></WhyNeedBranding>
             <OurPortfolio></OurPortfolio>
 
         </div>
