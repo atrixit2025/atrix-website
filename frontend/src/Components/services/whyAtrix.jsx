9afc0075a15bc21be64603a2dac65c5f4238f45e
Anil Verma <rajat.asoftech@gmail.com>
2025-04-10 17:58:03 +0530

    changes

--- frontend/src/Components/services/whyAtrix.jsx
+++ frontend/src/Components/services/whyAtrix.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const whyAtrix = () => {
   return (
     <div className='why-atrix'>
-        <div className='container ' ></div>
+        <div className='container  ' ></div>
 
     </div>
   )
