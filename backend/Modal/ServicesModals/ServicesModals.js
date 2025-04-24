2b84c680d6319b9fea4ec8f95c1a18691eca540c
atrixit2025 <rajat.asoftech@gmail.com>
2025-04-25 02:20:46 +0530

    add fields

--- backend/Modal/ServicesModals/ServicesModals.js
+++ backend/Modal/ServicesModals/ServicesModals.js
@@ -1,17 +1,25 @@
 import mongoose from "mongoose";
 
 const HeadercontentSectionSchema = new mongoose.Schema({
-    centerHeading:{type:String},
+    centerHeading: { type: String },
     centerDescription: { type: String },
 
     headingAnddescription:
         [{
             heading: { type: String },
             description: { type: String },
-              imageId: { type: String }, 
+            imageId: { type: String },
 
         }],
+
+});
+
+const textandimageSectionSchema = new mongoose.Schema({
+    type: { type: String, enum: ['texttoimage', 'imagetotext'], required: true },
  
+        text: { type: String },
+        imageId: { type: String }
+
 });
 
 const WhydoNeedSectionSchema = new mongoose.Schema({
@@ -38,9 +46,9 @@ const BannerfieldSectionSchema = new mongoose.Schema({
         enum: ['banner', 'video', 'slider'],
         required: true
     },
-    imageId: { type: String }, 
-    sliderImages: [{ type: String }] 
-}, { _id: true }); 
+    imageId: { type: String },
+    sliderImages: [{ type: String }]
+}, { _id: true });
 
 
 
@@ -49,6 +57,12 @@ const ServicesSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true
+    },
+    Slug: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
     },
     description: {
         type: String,
@@ -69,20 +83,22 @@ const ServicesSchema = new mongoose.Schema({
     WhydoNeed: [WhydoNeedSectionSchema],
     Process: [ProcessSectionSchema],
     WhyAtrix: [WhyAtrixSectionSchema],
-    Headercontent:[HeadercontentSectionSchema],
-    gallery:[{
+    Headercontent: [HeadercontentSectionSchema],
+    gallery: [{
         imageId: { type: String }
     }],
-    texttoimageandimagetotext:{
-          lefttext:{type: String},
-          rightimageId:{type: String},
-          righttext:{type: String},
-          leftimageId:{type: String}
-    },
+    texttoimageandimagetotext:
+    [
+        textandimageSectionSchema
+    ],
     portfolioCategories: {
         type: [String],
         default: []
     },
+    Technology: [{
+        title: { type: String },
+        imageId: { type: String }
+    }],
     faqCategories: {
         type: [String],
         default: []
@@ -94,6 +110,10 @@ const ServicesSchema = new mongoose.Schema({
     FeaturedImage: {
         type: String,
         required: true
+    },
+    Cta:{
+        title:{ type: String },
+        description: {type: String },
     },
     updatedAt: {
         type: Date,
