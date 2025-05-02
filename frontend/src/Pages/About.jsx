d6ca95c1ea14d7d59c6b002f94eef1a5f35bd1d1
Anil Verma <rajat.asoftech@gmail.com>
2025-05-02 18:59:13 +0530

    changes

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -29,6 +29,7 @@ import Behance from "../Components/Behanceshowpost/Behance";
 
 import thumbnail from "../assets/thumbnail/t.png";
 import { RxArrowTopRight } from "react-icons/rx";
+import LinkButton from "../Components/LinkButton";
 
 
 const cards = [
@@ -171,9 +172,11 @@ const About = () => {
           {/* Button Section */}
 
           <div className="md:col-span-2 col-span-12 flex justify-center items-center md:justify-end mt-4 md:mt-0 ">
-            <Link to='/contact-us' className="bg-(--white)  font-extrabold text-(--blue) px-4 py-2  duration-300  rounded-lg  cursor-pointer  flex items-center gap-2 group hover:scale-104  ">Contact us!<span className="  ">
+
+            {/* <Link to='/contact-us' className="bg-(--white)  font-extrabold text-(--blue) px-4 py-2  duration-300  rounded-lg  cursor-pointer  flex items-center gap-2 group hover:scale-104  ">Contact us!<span className="  ">
               <RxArrowTopRight className=" group-hover:rotate-45 group-hover:scale-140 duration-250 " />
-            </span></Link>
+            </span></Link> */}
+            <LinkButton mybtn={'Contact us!'}  btnLink={'/contact-us'}   >Contact us!</LinkButton>
 
           </div>
 
